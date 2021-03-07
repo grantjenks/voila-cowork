ec89958b337a2f07d2f60de47030f2306bd37b64
Grant Jenks <grant.jenks@gmail.com>
2021-03-07 00:06:46 -0800

    Add prototype comment form

--- cowork/views.py
+++ cowork/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import CommentForm
+from .models import Comment
 
 
 def comments(request):
-    return render(request, 'cowork/comments.html')
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment_form.save()
+            return redirect('comments')
+    else:
+        comment_form = CommentForm()
+    comments = Comment.objects.all().order_by('-create_time')
+    return render(
+        request,
+        'cowork/comments.html',
+        {'comment_form': comment_form, 'comments': comments},
+    )
